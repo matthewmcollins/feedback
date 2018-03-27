325fe7a9cc3100d35c864ab25fecc39a8bd24359
matthewmcollins <mmcollins27@gmail.com>
2018-03-27 16:18:23 -0500

    fixed keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //Figure out what set of credentials to return
-if (proces.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   //return prod keys
   module.exports = require("./prod");
 } else {
