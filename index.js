ced8ce4ab4093108030b6c0c214d6c58fd2f7c5f
matthewmcollins <mmcollins27@gmail.com>
2018-04-14 01:31:52 -0500

    Added build steps

--- index.js
+++ index.js
@@ -21,5 +21,20 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  // this happens first
+  app.use(express.static("client/build"));
+
+  //Express will serve up the index.html file
+  //if it doesn't know the route
+  //this is the catch all (all previous attempts to match a resource has failed)
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
