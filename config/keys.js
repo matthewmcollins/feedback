a00b65110f3a3c8265afb30f539cdb08cd02ee33
matthewmcollins <mmcollins27@gmail.com>
2018-04-15 23:56:51 -0500

    Creating surveys and send email via sendGrid

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 //Figure out what set of credentials to return
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   //return prod keys
-  module.exports = require("./prod");
+  module.exports = require('./prod');
 } else {
   //return dev keys
-  module.exports = require("./dev");
+  module.exports = require('./dev');
 }
